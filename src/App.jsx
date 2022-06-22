0d4232bb2ec96332240362dabd9b17313ee6440e
B-Lucas365 <lucasrenan365@outlook.com>
2022-06-22 10:47:50 -0300

    Implementado carrossel de pokemons com a lib swiper

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useRef, useState} from 'react'
 
 import {Header} from './components/Header'
 import {Pokecard} from './components/Pokecard'
 import './App.scss'
-import left from './assets/left.svg'
-import right from './assets/right.svg'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import "swiper/css";
+import "swiper/css/navigation";
+
+import "./styles.css";
+import { Navigation } from "swiper";
+
 
 
 export const App = () => {
   const [allPokemons, setAllPokemons] = useState([])
-  const carousel = useRef(null)
 
   const loadPokemons = async () => {
     const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=150')
@@ -30,39 +34,32 @@ export const App = () => {
     loadPokemons()
   }, [])
 
-  const handleLeftCick = (e) => {
-    e.preventDefault()
-    carousel.current.scrollLeft -= carousel.current.offsetWidth
-
-  }
-
-  const handleRightClick = (e) => {
-    e.preventDefault()
-    carousel.current.scrollLeft += carousel.current.offsetWidth
-
-  }
-
   return(
     <div className='container'>
       <Header />
       
       <div className='pokedex' >
-        <section className='pokebox' ref={carousel}>
+        <Swiper 
+        navigation={true} 
+        modules={[Navigation]} 
+        className="mySwiper" 
+        slidesPerView={4}
+        
+        >
+        
           {allPokemons.map((item, index) => (
-            <Pokecard 
-            name={item.name}
-            image={item.sprites.other.dream_world.front_default}
-            type={item.types[0].type.name}
-            id={item.id}
-            weight={item.weight}
-            xp={item.base_experience}
-            />
+            <SwiperSlide>
+              <Pokecard 
+              name={item.name}
+              image={item.sprites.other.home.front_default}
+              type={item.types[0].type.name}
+              id={item.id}
+              weight={item.weight}
+              xp={item.base_experience}
+              />
+            </SwiperSlide>
           ))}
-        </section>
-        <div className='control'>
-            <button  onClick={handleLeftCick}><img src={left} alt="" /></button>
-            <button onClick={handleRightClick}><img src={right} alt="" /></button>
-          </div>
+        </Swiper>
       </div>
     </div>
   )
--- src/components/Pokecard/index.jsx
+++ src/components/Pokecard/index.jsx
@@ -4,31 +4,33 @@ import './style.scss'
 
 export const Pokecard = ({name,image, type, id,weight, xp}) => {
     return (
-        <div className='card'>
-            <section className='pokemon'>
-                <div>
-                    <img src={image} alt="" />
-                </div>
+        <div className="cardContainer">
+            <div className='card'>
+                <section className='pokemon'>
+                    <div>
+                        <img src={image} alt="" />
+                    </div>
 
-                <div className='name-type'>
-                    <p>{name}</p>
-                    <span> </span>
-                </div>
-            </section>
+                    <div className='name-type'>
+                        <p>{name[0].toUpperCase() + name.substring(1)}</p>
+                        <span> </span>
+                    </div>
+                </section>
 
-            <section className='info'>
-                <div className='xp'>
-                    <p>XP</p>
-                    <p>{xp}</p>
-                </div>
-                <div className='weight'>
-                    <p>Weight</p>
-                    <p>{weight}</p>
-                </div>
-            </section>
+                <section className='info'>
+                    <div className='xp'>
+                        <p>XP</p>
+                        <p>{xp}</p>
+                    </div>
+                    <div className='weight'>
+                        <p>Weight</p>
+                        <p>{weight}</p>
+                    </div>
+                </section>
 
-            <div className='id'>
-                #{id < 10 ? '00' + id : '0' + id}
+                <div className='id'>
+                    #{id < 10 ? '00' + id : '0' + id}
+                </div>
             </div>
         </div>
     )
